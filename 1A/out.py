62af7d0e60f9d581e7f48f465b3a42ec4526259f
spedar <46629325+spedar@users.noreply.github.com>
2022-02-28 15:47:17 +0530

    Delete 1A directory

--- 1A/out.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-num = int( sys.argv[1])
-sent = sys.argv[2:]
-f = open('out.txt','r')
-ans = f.read().splitlines()[num].split()
-f.close()
-sent = ' '.join(sent)
-print(sent.replace("XXX", ans[3]))
--- 1A/out2.py
+++
@@ -1,5 +0,0 @@
-
-f = open('out.txt','r')
-ans = f.read().splitlines()
-sent = [i.split()[3] for i in ans]
-print(' '.join(sent[1:-1]))
